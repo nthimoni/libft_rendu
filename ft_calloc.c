c8c3571abfc5b22b3f2f6cf70055885dcda638bb
nthimoni <thimoniern@gmail.com>
2021-11-24 13:55:26 +0100

    calloc

---
+++ ft_calloc.c
@@ -0,0 +1,10 @@
+
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	return (malloc(nmemb * size));
+}
--- libft.h
+++ libft.h
@@ -24,6 +24,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int	ft_atoi(const char *str);
-char *ft_strdup(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 
 #endif
