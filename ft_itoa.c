7b7db1389992870131a3e7a0fa3b76c87f807771
Nicolas THIMONIER <nthimoni@e2r8p7.clusters.42paris.fr>
2021-11-24 19:22:38 +0100

    uwu

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:58:26 by nthimoni          #+#    #+#             */
-/*   Updated: 2021/11/24 18:52:37 by nthimoni         ###   ########.fr       */
+/*   Updated: 2021/11/24 19:22:31 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,23 @@
 
 size_t	nb_size(int a)
 {
-	size_t	
+	size_t	len;
+
+	len = 0;
+	while (a > 0)
+	{
+		a /= 10;
+		len++;
+	}
+	return (len);
 }
 
 char	*ft_itoa(int n)
 {
-	
+	size_t	len;
+	char	*ret;
+
+	len = nb_size(n);
+	ret = malloc(len + 1);
+
 }
